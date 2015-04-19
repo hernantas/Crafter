93f65eb893b50d2b355e5fe085f46239a5367e64
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-20 00:30:21 +0700

    Add 3 default monster to Player

--- Assets/AssetsReference.cs
+++ Assets/AssetsReference.cs
@@ -6,13 +6,21 @@ public class AssetsReference : MonoBehaviour
 {
 	public GUIStyle textStyle = new GUIStyle();
 	public List<GameObject> monsterTemplate = new List<GameObject>();
+	public GameObject textUtility;
 
 	public AssetsReference()
+	{
+
+	}
+
+	void Start()
 	{
 		if (monsterTemplate.Count > 0 && !PlayerMonster.IsLoaded)
 		{
 			PlayerMonster.Load();
-			PlayerMonster.Add(monsterTemplate[0].name, 1);
+			PlayerMonster.Add(monsterTemplate[0].name, 0);
+			PlayerMonster.Add(monsterTemplate[1].name, 0);
+			PlayerMonster.Add(monsterTemplate[2].name, 0);
 		}
 	}
 }
