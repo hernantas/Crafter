02bff8a2d9d79e1f580774560fa31916aad7271f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-20 00:31:57 +0700

    Add gameObject creator to Reference

--- Assets/Reference.cs
+++ Assets/Reference.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public static class Reference
 {
-	public static AssetsReference Get
+	public static AssetsReference Asset
 	{
 		get
 		{
 			return GameObject.FindGameObjectWithTag("Reference").GetComponent<AssetsReference>();
+		}
+	}
+
+	public static MonsterCreator Creator
+	{
+		get
+		{
+			return  GameObject.FindGameObjectWithTag("Reference").GetComponent<MonsterCreator>();
 		}
 	}
 }
