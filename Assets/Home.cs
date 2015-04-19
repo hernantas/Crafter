19e0265e0e3410f98eef1b76b174f32423f3a6e5
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-20 00:31:06 +0700

    Add home button

---
+++ Assets/Home.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Home : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+			RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), 
+			                                     Vector2.zero);
+			
+			if (hit.transform != null && hit.transform.gameObject == this.gameObject)
+			{
+				Application.LoadLevel(0);
+			}
+		}
+	}
+}
