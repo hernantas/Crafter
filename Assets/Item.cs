a104fb4390f6691a47ba0a8e387fb972ddd6c445
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-22 17:40:56 +0700

    Add effect type to items

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum ItemEffect
+{
+	EFFECT_NONE,
+	EFFECT_HEAL,
+	EFFECT_DAMAGE,
+	EFFECT_DAMAGE_AMP
+}
+
 public class Item : MonoBehaviour 
 {
+	[SerializeField]
+	private ItemEffect effect = new ItemEffect();
+
+	[SerializeField]
+	private float value;
+	public float Value
+	{
+		get { return value; }
+	}
+
+	[SerializeField]
+	private float duration;
+	public float Duration
+	{
+		get { return duration; }
+	}
+
 	[SerializeField]
 	private int cost = 0;
 	public int Cost
 	{
 		set { cost = value; }
 		get { return cost; }
-	}
-
-
-
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
 	}
 }
