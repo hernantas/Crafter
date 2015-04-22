01928b3ea6aa7e89abf1215c3d6523a4c2f63293
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-22 17:41:35 +0700

    Add player items static class

--- Assets/ItemShop.cs
+++ Assets/ItemShop.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ItemShop : MonoBehaviour {
+public class ItemShop : MonoBehaviour 
+{
+	private List<GameObject> colliderList = new List<GameObject>();
+	[SerializeField]
+	private GameObject colliderTemplate = null; 
 
 	// Use this for initialization
 	void Start () 
@@ -19,9 +23,10 @@ public class ItemShop : MonoBehaviour {
 	public void DisplayItem()
 	{
 		Vector3 offset = new Vector3(-2.52f, 1.33f);
+		Vector3 textOffset = new Vector3(0.2f,-0.1f) + offset;
 
 		List<GameObject> templates = Reference.Asset.itemTemplate;
-		Debug.Log(templates.Count);
+
 		for (int i=0;i<templates.Count;i++)
 		{
 			GameObject item = (GameObject) Instantiate(templates[i],
@@ -29,6 +34,21 @@ public class ItemShop : MonoBehaviour {
 			                                           Quaternion.identity);
 			//item.transform.parent = this.transform;
 			item.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+			GameObject cost = (GameObject) Instantiate(Reference.Asset.textUtility,
+			                                           new Vector3(i%6,-i/6,-2)+textOffset,
+			                                           Quaternion.identity);
+			cost.GetComponent<TextMesh>().text = (item.GetComponent<Item>().Cost).ToString();
+			cost.GetComponent<TextMesh>().fontSize = 22;
+			cost.GetComponent<TextMesh>().color = new Color(0,0,0);
+
+			GameObject collider = (GameObject) Instantiate(colliderTemplate,
+			                                               new Vector3(i%6,-i/6,0)+offset,
+			                                               Quaternion.identity);
+
+			colliderList.Add(collider);
 		}
+
+
 	}
 }
---
+++ Assets/PlayerItems.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class PlayerItems 
+{
+	private static Dictionary<string, int> items = new Dictionary<string, int>();
+
+	public static void Add(string itemName, int quantity)
+	{
+		if (!items.ContainsKey(itemName))
+		{
+			items[itemName] = 0;
+		}
+
+		items[itemName] += quantity; 
+	}
+}
