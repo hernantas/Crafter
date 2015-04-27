3bbf239fefc88226c4f4ba793377d143c6ca9196
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-28 01:51:17 +0700

    Player item now can access to Count, dictionary and can get quantity

--- Assets/Scripts/PlayerItems.cs
+++ Assets/Scripts/PlayerItems.cs
@@ -5,6 +5,15 @@ using System.Collections.Generic;
 public static class PlayerItems 
 {
 	private static Dictionary<string, int> items = new Dictionary<string, int>();
+	public static Dictionary<string, int> Items
+	{
+		set { items = value; }
+		get { return items; }
+	}
+	public static int Count
+	{
+		get { return items.Count; }
+	}
 
 	public static void Add(string itemName, int quantity)
 	{
@@ -14,5 +23,13 @@ public static class PlayerItems
 		}
 
 		items[itemName] += quantity; 
+	}
+
+	public static int Get(string itemName)
+	{
+		if (items.ContainsKey(itemName))
+			return items[itemName];
+
+		return 0;
 	}
 }
