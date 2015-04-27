b31be659780e35121a74a39bd71a445f1f111f30
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-28 01:52:41 +0700

    Prepare for item click, not used for now

---
+++ Assets/Scripts/ItemClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemClick : MonoBehaviour 
+{
+	private bool duelStage = false;
+	public bool DuelStage 
+	{ 
+		set { duelStage = value; }
+		get { return duelStage; }
+	}
+
+	public string ItemData { set; get; }
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseUp()
+	{
+
+	}
+}
