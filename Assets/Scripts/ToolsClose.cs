afd1b4e6bfbe4e209134d3ecdb155b7b8437faa2
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-04-28 01:52:05 +0700

    Show player item when playing

---
+++ Assets/Scripts/ToolsClose.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolsClose : MonoBehaviour 
+{
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+
+	void OnMouseUp()
+	{
+		ToolsDuel td = this.transform.parent.GetComponent<ToolsDuel>();
+		td.Hide();
+	}
+}
